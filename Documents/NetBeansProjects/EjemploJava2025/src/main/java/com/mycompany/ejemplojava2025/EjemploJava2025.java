796cad6074b75439139d080e43f7a2eb2ef16930
Cayoka <cayoka.mp@gmail.com>
2025-02-24 19:49:55 +0100

    Update EjemploJava2025.java

--- Documents/NetBeansProjects/EjemploJava2025/src/main/java/com/mycompany/ejemplojava2025/EjemploJava2025.java
+++ Documents/NetBeansProjects/EjemploJava2025/src/main/java/com/mycompany/ejemplojava2025/EjemploJava2025.java
@@ -11,6 +11,6 @@ package com.mycompany.ejemplojava2025;
 public class EjemploJava2025 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Adrian");
+        System.out.println("Hola Adrian escribe aquí algo más");
     }
 }
